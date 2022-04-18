bff963eb8013b67a06441493da8a4ad5463a343a
User <user@Users-MacBook-Air.local>
2022-04-18 15:23:00 +0100

    done with get all comment by post id

--- src/main/java/com/deenn/springbootblogrestapi/controllers/CommentController.java
+++ src/main/java/com/deenn/springbootblogrestapi/controllers/CommentController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class CommentController {
@@ -20,5 +22,10 @@ public class CommentController {
     public ResponseEntity<CommentDto> createComment(@PathVariable Long postId, @RequestBody CommentDto commentDto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(commentService.createComment(postId, commentDto));
 
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<List<CommentDto>> getCommentsByPostId(@PathVariable Long postId) {
+        return ResponseEntity.ok(commentService.getCommentsByPostId(postId));
     }
 }
--- src/main/java/com/deenn/springbootblogrestapi/repositories/CommentRepository.java
+++ src/main/java/com/deenn/springbootblogrestapi/repositories/CommentRepository.java
@@ -3,5 +3,10 @@ package com.deenn.springbootblogrestapi.repositories;
 import com.deenn.springbootblogrestapi.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByPostId(Long postId);
+
 }
